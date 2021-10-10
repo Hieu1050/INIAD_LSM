b384ce9d6c563fc4fdacb47adff99aa2d1acb11d
Hieu Chi Vu <s1f101900060@iniad.org>
2021-10-10 16:36:16 +0900

    sample db lesson

---
+++ server/create_lesson.sql
@@ -0,0 +1,14 @@
+INSERT INTO api_lesson (lesson_name, date_start, date_end, course_id_id)
+VALUES 
+('01-CPU', '20211112', '20211212', 1),
+('02-Memory', '20211212', '20211212', 1),
+('03-Race Condition', '20211112', '20211212', 1),
+('Visualization', '20211112', '20211212', 2),
+('Statistical Test Review', '20211112', '20211212', 2),
+('Naive Bayes Classifier', '20211112', '20211212', 2),
+('Guidance', '20211112', '20211212',3),
+('Digital Representation Of Information','20211112', '20211212',3),
+('Information Design','20211112', '20211212',3),
+('Financial Statement','20211112', '20211212',4),
+('Investing and Financing','20211112', '20211212',4)
+;
